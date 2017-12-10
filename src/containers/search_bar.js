9e32a655ffc8e92738615d91333d3886c9bdf299
Juan Salvatore <juansalvatore@live.com.ar>
2017-12-10 20:05:29 -0300

    Create Search bar component

---
+++ src/containers/search_bar.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+export default class SearchBar extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            term: ''
+        }
+
+        this.onInputChange = this.onInputChange.bind(this)
+    }
+
+    onInputChange(event) {
+        this.setState({ term: event.target.value })
+    }
+
+    render() {
+        return (
+            <form className="input-group">
+                <input 
+                    placeholder="Get a five day forecast in your favorite cities"
+                    className="form-control"
+                    value={ this.state.term } 
+                    onChange={this.onInputChange} />
+
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            </form>
+        )
+    }
+}
+
+// function mapStateToProp(state) {
+//     return 
+// }
+
+// export default connect(mapStateToProp)(SearchBar)
