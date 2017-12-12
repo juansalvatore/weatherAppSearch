f96e40cac5a06620ac9c16c7a715f23fc467d8f7
Juan Salvatore <juansalvatore@live.com.ar>
2017-12-11 23:33:53 -0300

    Add action fetch weather

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,6 +11,8 @@ export function fetchWeather(city) {
 
     const request = axios.get(url)
 
+    console.log('Request: ', request)
+
     return {
         type: FETCH_WEATHER,
         payload: request
