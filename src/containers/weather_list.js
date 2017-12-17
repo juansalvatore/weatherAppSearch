439238f9d8dab9ddcdccbb82ec22adba3f997e5d
Juan Salvatore <juansalvatore@live.com.ar>
2017-12-17 14:22:03 -0300

    adjust styles and temperature unit

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -5,7 +5,10 @@ import Chart from '../components/chart'
 class WeatherList extends Component {
   renderWeather = cityData => {
     const name = cityData.city.name
-    const temps = cityData.list.map(weather => weather.main.temp)
+    const temps = _.map(
+      cityData.list.map(weather => weather.main.temp),
+      temp => temp - 273
+    )
     const pressures = cityData.list.map(weather => weather.main.pressure)
     const humidities = cityData.list.map(weather => weather.main.humidity)
 
@@ -13,13 +16,13 @@ class WeatherList extends Component {
       <tr key={name}>
         <td>{name}</td>
         <td>
-          <Chart data={temps} color="orange" />
+          <Chart data={temps} color="orange" units="C" />
         </td>
         <td>
-          <Chart data={pressures} color="green" />
+          <Chart data={pressures} color="green" units="hPa" />
         </td>
         <td>
-          <Chart data={humidities} color="blue" />
+          <Chart data={humidities} color="blue" units="%" />
         </td>
       </tr>
     )
@@ -31,9 +34,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (C)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
